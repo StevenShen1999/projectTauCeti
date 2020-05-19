3c4f66b50be8de3d81c0910fa9074c277f120ad3
Steven Shen <stevenshen1999@hotmail.com>
2020-05-19 22:22:03 +0930

    Slightly modified util/backupServices.py to check for more conditions

--- backend/util/backupServices.py
+++ backend/util/backupServices.py
@@ -2,4 +2,11 @@ import os
 from datetime import datetime
 
 currTime = datetime.now().strftime("%d%m%Y_%H%M")
+
+if os.system("cd ~/databaseBackups >/dev/null 2>&1") != 0:
+    print("Path doesn't exist, creating ~/databaseBackups")
+    os.system("mkdir ~/databaseBackups")
+    print("Path created")
+
 os.system(f"pg_dump tauCetiDB > ~/databaseBackups/{currTime}.bak")
+print(f"Databased backup copy created in ~/databaseBackups/{currTime}.bak")
